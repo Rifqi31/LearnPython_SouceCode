5e3ba73a6ef4ca2371f5b90aeb4a34a3cecb856b
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-10 14:18:01 +0700

    add while example with n counter is 0

---
+++ fundamental/chapter_3/while_example_3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# File Name : while_example_3.py
+
+
+def main():
+    """
+        Bab Statement Kontrol
+        Bagian Struktur Pengulangan
+        Statement While
+    """
+
+    # melakukan perulangan
+    nCounter = 0
+    while nCounter < 10:
+        print("Baris ke-%d Hello World" % nCounter)
+        nCounter += 1
+
+
+if __name__ == "__main__":
+    main()
