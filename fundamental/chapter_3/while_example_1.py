987cd205f10f251857b8b773a5e9bbb47a2ef343
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-10 13:31:46 +0700

    add while with simple case

---
+++ fundamental/chapter_3/while_example_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# File Name : while_example_1.py
+
+
+def main():
+    """
+        Bab Statement Kontrol
+        Bagian Struktur Pengulangan
+        Statement while
+    """
+
+    # menentukan banyaknya pengulangan
+    n = 5
+
+    # melakukan pengulangan
+    i = 1
+    while i <= n:
+        print(i)
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
