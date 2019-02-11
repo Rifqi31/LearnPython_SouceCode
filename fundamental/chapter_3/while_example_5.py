7bb12b051c51401ffe15b326bae99034121573f5
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-11 12:14:41 +0700

    add while example with decrement counter

---
+++ fundamental/chapter_3/while_example_5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# File Name : while_example_5.py
+
+
+def main():
+    """
+        Bab Statement Kontrol
+        Bagian Struktur Perulangan
+        Statement while
+    """
+
+    # melakukan perulangan
+    nCounter = 6
+    while nCounter >= 1:
+        print("%d : Hello World!" % nCounter)
+        nCounter -= 1
+
+
+if __name__ == "__main__":
+    main()
