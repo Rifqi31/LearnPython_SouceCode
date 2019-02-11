bc7f59156c8819d76d598efb1b4ac22c45e53763
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-11 12:52:48 +0700

    add for example with string type iteration

---
+++ fundamental/chapter_3/for_example_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# File Name : for_example_1.py
+
+
+def main():
+    """
+        Bab Statement Kontrol
+        Bagian Statement Perulangan
+        Statement for
+    """
+
+    # menggunakan for pada tipe string
+    for i in 'python':
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
