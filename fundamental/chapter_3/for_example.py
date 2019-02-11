7bca1f96224e246cdf8ec481ed06f9819f33c8bd
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-11 12:51:27 +0700

    add for example with range() method

---
+++ fundamental/chapter_3/for_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# File Name : for_example.py
+
+
+def main():
+    """
+        Bab Statement Kontrol
+        Bagian Statement Perulangan
+        Statement for
+    """
+
+    # menggunakan for untuk rentang nilai tertentu
+    for nRead in range(1, 6):
+        print("%d: Hello World!" % nRead)
+
+
+if __name__ == "__main__":
+    main()
